83d300a49e9a2335f453f2d0bfd9530359092327
kimba <skyqwn@gmail.com>
2024-01-22 20:50:11 +0900

    front cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,12 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  app.enableCors({
+    origin: ['https://studio.apollographql.com', 'http://localhost:3000'], // 허용할 출처
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', // 허용할 HTTP 메서드
+    allowedHeaders: 'Content-Type, Accept', // 허용할 헤더
+    credentials: true,
+  });
+  await app.listen(4000);
 }
 bootstrap();
